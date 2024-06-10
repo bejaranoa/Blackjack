4c517cdb6769c817cd375a5813312dc214ff3b25
bejaranoa <bjaranoa@stu.otc.school.nz>
2024-06-10 13:14:57 +1200

    I positioned the labels for the player/dealer

--- Blackjack.py
+++ Blackjack.py
@@ -1,12 +1,18 @@
 import customtkinter as ctk
+from PIL import Image
 import random
 import cards
 
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("blue")
 root = ctk.CTk()
-root.geometry('400x240')
+root.geometry('700x500')
+root.minsize(700, 500)
+root.maxsize(700,500)
 
+root.configure()
+
+ryanpoop = ctk.CTkImage(Image.open("anything.jpg"), size=(200, 200))
 
 def get_random_card():
     return list(random.choice(list(cards.data.items())))[1]
@@ -17,11 +23,13 @@ def play_button():
 
     frame = ctk.CTkFrame(root)
     player_label = ctk.CTkLabel(frame, text=f'player card: {player_card}')
-    dealer_label = ctk.CTkLabel(frame, text=f'dealer card: {dealer_card}')
+    dealer_label = ctk.CTkLabel(frame, text=f'dealer card: {dealer_card}',width=0.1)
     frame.pack(fill="both", expand=1)
-    player_label.pack(side="left", padx=0.2, pady=0.2, fill="x")
-    dealer_label.pack(side="top", padx=0.5, pady=0.5)
-    dealer_label.pack()
+    player_label.grid(row=6, column=0)
+    dealer_label.grid(row=0, column =0)
+
+    image_label = ctk.CTkLabel(frame, text="", image=ryanpoop)
+    image_label.grid(row=1, column=3, padx=10)
 
 button = ctk.CTkButton(master=root, text="Play", command=play_button)
 button.place(relx=0.5, rely=0.40, anchor=ctk.CENTER)
@@ -32,5 +40,3 @@ quit.place(relx=0.5, rely=0.60, anchor=ctk.CENTER)
 
 
 root.mainloop()
-
-
