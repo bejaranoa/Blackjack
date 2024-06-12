40a6bfdd5a07ecd64e3e470f38e833704e7c77bf
bejaranoa <bjaranoa@stu.otc.school.nz>
2024-06-12 14:31:23 +1200

    Fixed the text placement

--- Blackjack.py
+++ Blackjack.py
@@ -12,8 +12,6 @@ root.maxsize(700,500)
 
 root.configure()
 
-ryanpoop = ctk.CTkImage(Image.open("anything.jpg"), size=(200, 200))
-
 def get_random_card():
     return list(random.choice(list(cards.data.items())))[1]
 
@@ -23,13 +21,13 @@ def play_button():
 
     frame = ctk.CTkFrame(root)
     player_label = ctk.CTkLabel(frame, text=f'player card: {player_card}')
-    dealer_label = ctk.CTkLabel(frame, text=f'dealer card: {dealer_card}',width=0.1)
+    dealer_label = ctk.CTkLabel(frame, text=f'dealer card: {dealer_card}',
+                                width=0.1)
+    ask = ctk.CTkLabel(frame, text='Hit or stand?')
     frame.pack(fill="both", expand=1)
-    player_label.grid(row=6, column=0)
-    dealer_label.grid(row=0, column =0)
-
-    image_label = ctk.CTkLabel(frame, text="", image=ryanpoop)
-    image_label.grid(row=1, column=3, padx=10)
+    player_label.place(relx=0.06, rely = 0.5, anchor=ctk.CENTER)
+    dealer_label.place(relx= 0.06, rely = 0.02, anchor=ctk.CENTER)
+    ask.place(relx=0.5, rely = 0.5, anchor=ctk.CENTER)
 
 button = ctk.CTkButton(master=root, text="Play", command=play_button)
 button.place(relx=0.5, rely=0.40, anchor=ctk.CENTER)
