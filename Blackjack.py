1e82453357b95bde0d6169c4aa75ff42ead818ed
bejaranoa <bjaranoa@stu.otc.school.nz>
2024-06-19 15:13:00 +1200

    Play again and quit button

--- Blackjack.py
+++ Blackjack.py
@@ -4,8 +4,8 @@ import random
 import cards
 import time
 
-ctk.set_appearance_mode("light")
-ctk.set_default_color_theme("blue")
+ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
 
 def get_key_from_value(dict, v) -> str:
     for key, value in dict.items():
@@ -28,44 +28,45 @@ class App(ctk.CTk):
         self.dealer_cards = [get_random_card()]
         self.player_cards = [get_random_card(), get_random_card()]
 
-        def check_player_score():
-            if self.get_player_total() > 21:
-                self.frame.destroy()
-                self.display_game_status("You Busted ;)")
-
-        def increment_player_score():
-            self.player_cards.append(get_random_card())
-            check_player_score()
-            self.player_label.configure(text=f'player card: {self.concatenate_player_cards()} = {self.get_player_total()}')
-
-        def increment_dealer_score():
-            dealer_total = self.get_dealer_total()
-            while dealer_total < 17:
-                self.dealer_cards.append(get_random_card())
-                dealer_total = self.get_dealer_total()
-                self.dealer_label.configure(text=f'dealer card: {dealer_total}')
-                time.sleep(0.8)
-            self.finish()        
-
-        def play_button():
-            self.frame = ctk.CTkFrame(self)
-            self.player_label = ctk.CTkLabel(self.frame, 
-            text=f'player card: {self.concatenate_player_cards()} = {self.get_player_total()}')
-            self.dealer_label = ctk.CTkLabel(self.frame, 
-                            text=f'dealer card: {self.get_dealer_total()}',width=0.1)
-            self.frame.pack(fill="both", expand=1)
-            self.player_label.place(relx=0.06, rely = 0.5, anchor=ctk.CENTER)
-            self.dealer_label.place(relx= 0.06, rely = 0.02, anchor=ctk.CENTER)
-            hit=ctk.CTkButton(self.frame, text="HIT", command=increment_player_score)
-            hit.place(relx= 0.35, rely = 0.5, anchor=ctk.CENTER)
-            stand=ctk.CTkButton(self.frame, text='STAND', command=increment_dealer_score)
-            stand.place(relx=0.65, rely=0.5, anchor=ctk.CENTER)
-        
-        button = ctk.CTkButton(master=self, text="Play", command=play_button)
+        button = ctk.CTkButton(master=self, text="Play", command=self.play_button)
         button.place(relx=0.5, rely=0.40, anchor=ctk.CENTER)
 
-        quit = ctk.CTkButton(master=self, text="Quit", command=self.destroy)
-        quit.place(relx=0.5, rely=0.60, anchor=ctk.CENTER)
+        self.exit = ctk.CTkButton(master=self, text="Quit", command=self.destroy)
+        self.exit.place(relx=0.5, rely=0.60, anchor=ctk.CENTER)
+
+    def check_player_score(self):
+        if self.get_player_total() > 21:
+            self.frame.destroy()
+            self.display_game_status("You Busted ;)")
+
+    def increment_player_score(self):
+        self.player_cards.append(get_random_card())
+        self.check_player_score()
+        self.player_label.configure(text=f'player card: {self.add_player_cards()} = {self.get_player_total()}')
+
+    def increment_dealer_score(self):
+        dealer_total = self.get_dealer_total()
+        while dealer_total < 17:
+            self.dealer_cards.append(get_random_card())
+            dealer_total = self.get_dealer_total()
+            self.dealer_label.configure(text=f'dealer card: {dealer_total}')
+            time.sleep(0.8)
+        self.finish()
+
+
+    def play_button(self):
+        self.frame = ctk.CTkFrame(self)
+        self.player_label = ctk.CTkLabel(self.frame, 
+        text=f'player card: {self.add_player_cards()} = {self.get_player_total()}')
+        self.dealer_label = ctk.CTkLabel(self.frame, 
+                        text=f'dealer card: {self.get_dealer_total()}',width=0.1)
+        self.frame.pack(fill="both", expand=1)
+        self.player_label.place(relx=0.06, rely = 0.5, anchor=ctk.CENTER)
+        self.dealer_label.place(relx= 0.06, rely = 0.02, anchor=ctk.CENTER)
+        hit=ctk.CTkButton(self.frame, text="HIT", command=self.increment_player_score)
+        hit.place(relx= 0.35, rely = 0.5, anchor=ctk.CENTER)
+        stand=ctk.CTkButton(self.frame, text='STAND', command=self.increment_dealer_score)
+        stand.place(relx=0.65, rely=0.5, anchor=ctk.CENTER)
 
     def display_game_status(self, message: str):
         self.frame.destroy()
@@ -75,10 +76,21 @@ class App(ctk.CTk):
         self.status_label.place(relx=0.5,rely=0.5,anchor=ctk.CENTER)
 
     def finish(self):
-        if self.get_player_total() > self.get_dealer_total():
+        dealer_total = self.get_dealer_total()
+        player_total = self.get_player_total()
+        if dealer_total > 21 or player_total > dealer_total:
             self.display_game_status("YOU WIN ;P")
         else:
-           self.display_game_status("YOU LOSE ;(")
+            self.display_game_status(f"""YOU LOSE ;(          The Dealer Got {dealer_total}""")
+        play_again = ctk.CTkButton(text="Play again", command=self.status_frame.destroy,
+                                   master=self.status_frame)
+        play_again.place(relx=0.5, rely=0.6, anchor=ctk.CENTER)
+        done = ctk.CTkButton(text='Quit', command=self.destroy, 
+                             master=self.status_frame)
+        done.place(relx=0.5, rely=0.7, anchor=ctk.CENTER)
+        
+        
+        
 
     def get_dealer_total(self):
         total = 0
@@ -92,11 +104,13 @@ class App(ctk.CTk):
             total = total + card
         return total
 
-    def concatenate_player_cards(self):
+    def add_player_cards(self):
         cards = ""
         for card in self.player_cards:
             cards = cards + f' {str(card)}'
         return cards
+    
+    
     
 app = App()
 app.mainloop()
